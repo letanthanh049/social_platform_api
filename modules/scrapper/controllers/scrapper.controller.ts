d8913bb3942f207778153063d5595a0fa8b4e7a5
letanthanh <letanthanh049@gmail.com>
2023-08-10 18:11:21 +0700

    Update Scrapper with check tiktok account features

--- modules/scrapper/controllers/scrapper.controller.ts
+++ modules/scrapper/controllers/scrapper.controller.ts
@@ -1,6 +1,7 @@
 import { 
     Controller,
-    Get
+    Query,
+    Get,
 } from "@nestjs/common";
 import { ScrapperService } from "../services/scrapper.service";
 
@@ -8,8 +9,24 @@ import { ScrapperService } from "../services/scrapper.service";
 export class ScrapperController {
     constructor(private readonly scrapperService: ScrapperService) { }
 
-    @Get('fetch-data')
-    getDataFromWebsite() {
-        return this.scrapperService.getDataFromWebsite();
+    @Get('check-subcribe/youtube')
+    checkYoutubeSubcribe(
+        @Query('urlChannel') urlChannel: string,
+        @Query('subcriber')  subcriber: string
+    ) {
+        return this.scrapperService.checkYoutubeSubcribe(urlChannel, subcriber);
+    }
+
+    @Get('check-valid/tiktok/')
+    isValidTiktokAccount(
+        @Query('urlChannel') urlChannel: string,
+        @Query('code') code: string
+    ) {
+        return this.scrapperService.isValidTiktokAccount(urlChannel, code);
+    }
+
+    @Get('check-subcribe/tiktok')
+    checkTiktokSubcribe(@Query('urlChannel') urlChannel: string,) {
+        return this.scrapperService.checkTiktokSubcribe(urlChannel);
     }
 }
--- modules/scrapper/services/scrapper.service.ts
+++ modules/scrapper/services/scrapper.service.ts
@@ -11,29 +11,25 @@ export class ScrapperService {
         private readonly scrapperModel: mongoose.Model<Scrapper>
     ) { }
 
-    async getDataFromWebsite() {
+    async checkYoutubeSubcribe(urlChannel: string, subcriber: string) {
         const browser = await puppeteer.launch({ headless: 'new' });
         const page = await browser.newPage();
-        await page.setViewport({ width: 1280, height: 14000 })
-        await page.goto('https://www.youtube.com/@letanthanh9866/channels',);
+        await page.setViewport({ width: 1280, height: 14000 });
+        await page.goto(urlChannel);
         await page.evaluate(() => { window.scrollBy(0, window.innerHeight); });
         await page.keyboard.press('Backspace', { delay: 2000 });
-        const allChannelsEleHandle = await page.$$('#contents #items ytd-grid-channel-renderer');
-        const promises = allChannelsEleHandle.map(
-            async (eleHandle) => {
-                // return await eleHandle.evaluate((domElement) => domElement.outerHTML);
-                // return await eleHandle.$eval('#title', element => element.textContent)
-                return await eleHandle.$eval('#channel-info', element => element.getAttribute('href'))
+        const allChannelsElement = await page.$$('#contents #items ytd-grid-channel-renderer');
+        const promises = allChannelsElement.map(
+            async (channel) => {
+                // return await channel.evaluate((domElement) => domElement.outerHTML);
+                // return await channel.$eval('#title', element => element.textContent)
+                return await channel.$eval('#channel-info', element => element.getAttribute('href'))
             });
         const channels = await Promise.all(promises);
 
         let isExist = false;
         channels.forEach(ch => {
-            // if (ch.length == 'L'.length && ch === 'L') {
-            //     isExist = true;
-            //     return;
-            // }
-            if (ch === '/@TheKidLAROI') {
+            if (ch === subcriber) {
                 isExist = true;
                 return;
             }
@@ -41,4 +37,32 @@ export class ScrapperService {
         await browser.close();
         return isExist;
     };
+
+    async isValidTiktokAccount(urlChannel: string, code: string) {
+        const browser = await puppeteer.launch({ headless: 'new' });
+        const page = await browser.newPage();
+        await page.setViewport({ width: 1280, height: 480 });
+        await page.goto(urlChannel);
+        const userBioElement = await page.$('.tiktok-1g04lal-DivShareLayoutHeader-StyledDivShareLayoutHeaderV2');
+        const userBio = await userBioElement.$eval('.e1457k4r3', element => element.textContent);
+        let isValid = false;
+        if (userBio === code) isValid = true;
+        return isValid;
+    }
+
+    async checkTiktokSubcribe(urlChannel: string) {
+        const browser = await puppeteer.launch({ headless: false });
+        const page = await browser.newPage();
+        await page.setViewport({ width: 1280, height: 800});
+        await page.goto(urlChannel);
+        const followingListElement = await page.$$('.tiktok-j1fxk6-UlAccountList');
+        const promises = followingListElement.map(
+            async (followingUserElement) => {
+                return (await followingUserElement.getProperty('outerHTML'))
+            }
+        )
+        const userList = await Promise.all(promises); 
+        console.log(userList);
+        return userList;
+    }
 }
